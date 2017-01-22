3c01c397467f40c2ec41ac105f22be41b1068a5a
blackmahu <jk1402@163.com>
2017-01-23 02:13:04 +0800

    new file:   e3.32.scm

---
+++ Chapter3/e3.32.scm
@@ -0,0 +1,4 @@
+; The return of each procedure is the result of each change by time
+; as long as we use FIFO, the final result represent the final state.
+; if use LIFO, the last result of output is meaningless, just the result
+; of the first change, not the last.
