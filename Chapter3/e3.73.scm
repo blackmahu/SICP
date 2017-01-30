185b5f7f30b427cab39c2a6e908be7f042de0fdc
blackmahu <jk1402@163.com>
2017-01-31 03:26:00 +0800

    minor fix

--- Chapter3/e3.73.scm
+++ Chapter3/e3.73.scm
@@ -1,9 +1,9 @@
 (define (RC R C dt)
   (lambda (si v0)
 	(cons-stream v0
-				 (add-stream (scale-stream si R)
-							 (scale-stream
-							   (integral si 0 dt)
-							   (/ 1 C))))))
+				 (add-streams (scale-stream si R)
+							  (scale-stream
+								(integral si 0 dt)
+								(/ 1 C))))))
 
 (define RC1 (RC 5 1 0.5))
